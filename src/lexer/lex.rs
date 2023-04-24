71dffb8e6b212fc5f2df1e32000215a482c36a07
akarahdev <118059229+akarahdev@users.noreply.github.com>
2023-04-24 19:36:27 -0400

    finish cargo fmt, some files were missed

--- src/lexer/lex.rs
+++ src/lexer/lex.rs
@@ -17,12 +17,11 @@ pub fn string_to_tokens(input: String) -> Vec<Token> {
                 current_char += 1;
                 str_builder.push(ch);
 
-                
                 if let Some(token) = attempt_tokenize_str(&str_builder) {
                     stream.push(Token {
                         at_line: current_line,
                         at_char: current_char,
-                        token
+                        token,
                     });
                     str_builder = String::new();
                 }
@@ -50,34 +49,29 @@ pub fn tokenize_single_ch(input: char) -> Option<TokenType> {
         '{' => Some(OpenBraces),
         '}' => Some(CloseBraces),
         '=' => Some(Equals),
-        _ => None
+        _ => None,
     }
 }
 
 pub fn tokenize_identifier(input: &str) -> Option<TokenType> {
-    let valid_starters = ['A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G', 'H' , 
-        'I' , 'J' , 'K' , 'L' , 'M' , 'N', 'O' , 'P' , 
-        'Q' , 'R' , 'S' , 'T' , 'U', 'V' , 'W' , 'X' , 
-        'Y' , 'Z' , 'a' , 'b', 'c' , 'd' , 'e' , 'f' , 
-        'g' , 'h' , 'i', 'j' , 'k' , 'l' , 'm' , 'n' , 
-        'o' , 'p', 'q' , 'r' , 's' , 't' , 'u' , 'v' , 
-        'w', 'x' , 'y' , 'z', '_'];
-    let valid_continuers = ['A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G', 'H' , 
-        'I' , 'J' , 'K' , 'L' , 'M' , 'N', 'O' , 'P' , 
-        'Q' , 'R' , 'S' , 'T' , 'U', 'V' , 'W' , 'X' , 
-        'Y' , 'Z' , 'a' , 'b', 'c' , 'd' , 'e' , 'f' , 
-        'g' , 'h' , 'i', 'j' , 'k' , 'l' , 'm' , 'n' , 
-        'o' , 'p', 'q' , 'r' , 's' , 't' , 'u' , 'v' , 
-        'w', 'x' , 'y' , 'z' , '_' , '0' , '1' , '2' , 
-        '3' , '4' , '5' , '6' , '7' , '8' , '9'];
+    let valid_starters = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_',
+    ];
+    let valid_continuers = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_', '0',
+        '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    ];
     if !input.starts_with(valid_starters) {
         return None;
     }
     for ch in input.chars() {
         if !valid_continuers.contains(&ch) {
             return None;
-        }    
+        }
     }
     return Some(Identifier(input.to_string()));
-    
 }
--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,2 +1,2 @@
-pub mod tokens;
 pub mod lex;
+pub mod tokens;
