54b91ba1a4d5f4e5bbc3a799cfb23fb7774409f5
Camila <Camila.thirtytwo@gmail.com>
2023-04-28 11:06:40 -0700

    bls version
    - Added "version" command
    - misc cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use ariadne::*;
-use chumsky::Parser;
+#[allow(unused_imports)]
+use chumsky::{chain::Chain, Parser};
+#[allow(unused_imports)]
 use codegen::{block::Block, item::Item, item_data::ItemData, misc::process_block_vec};
 
-use std::{io::Write, net::TcpStream, env};
+use std::{env, io::Write, net::TcpStream};
 
 mod codegen;
 mod parser;
@@ -12,17 +14,22 @@ fn main() -> std::io::Result<()> {
 
     let args = env::args().collect::<Vec<_>>();
 
-    let input = "";
+    let _input = "";
 
-    let start = std::time::Instant::now();
+    let _start = std::time::Instant::now();
 
     if let Some(arg) = args.get(1) {
+        // command line commands
         if arg.contains("build-all") {
             let paths = std::fs::read_dir("./scripts")?;
 
             for path in paths {
                 let handle = std::thread::spawn(move || {
-                    let display = path.expect("somehow doesnt exist").path().display().to_string();
+                    let display = path
+                        .expect("somehow doesnt exist")
+                        .path()
+                        .display()
+                        .to_string();
                     let file = std::fs::read_to_string(display).expect("somehow doesnt exist");
                     process_inputs(&file);
                 });
@@ -34,11 +41,31 @@ fn main() -> std::io::Result<()> {
                 let file = std::fs::read_to_string(arg2)?;
                 process_inputs(&file);
             }
-        } 
-        
+        } else if arg.contains("version") {
+            //find the toml file - that has the version
+            //3rd line has version as `version = "[version]"
+            match std::fs::read_to_string("Cargo.toml")?.lines().nth(2) {
+                Some(line) => {
+                    let line = line.strip_prefix("version = ").unwrap_or("");
+                    if line.is_empty() {
+                        return Err(std::io::Error::new(
+                            std::io::ErrorKind::Other,
+                            "Did not find the correct version line.",
+                        ));
+                    }
+                    let vers = line.trim_matches('"');
+                    println!("Current version: {vers}")
+                }
+                None => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        "Could not find a valid `Cargo.toml` file",
+                    ))
+                }
+            }
+        }
     } else {
         help_message();
-        return Ok(());
     }
 
     Ok(())
@@ -65,8 +92,7 @@ fn process_inputs(input: &str) {
         Ok(vector) => {
             let vector = vector
                 .into_iter()
-                .filter(|f| f.is_some())
-                .map(|f| f.expect("impossible to fail"))
+                .flatten()
                 .collect::<Vec<_>>();
 
             compile_with_recode(vector);
@@ -77,7 +103,7 @@ fn process_inputs(input: &str) {
                     .with_message(format!("{:#?}", err.reason()))
                     .with_label(Label::new(err.span()).with_message(format!(
                         "expected '{}', found '{}'",
-                        err.expected().nth(0).unwrap_or(&Some('?')).unwrap_or('!'),
+                        err.expected().next().unwrap_or(&Some('?')).unwrap_or('!'),
                         err.found().unwrap_or(&'✗')
                     )))
                     .finish()
--- src/parser/parse.rs
+++ src/parser/parse.rs
@@ -2,7 +2,7 @@ use std::sync::{Arc, Mutex};
 
 use chumsky::prelude::*;
 
-use crate::codegen::{block::Block, item_data::ItemData, item::Item};
+use crate::codegen::{block::Block, item::Item, item_data::ItemData};
 
 pub fn parser() -> impl Parser<char, Vec<Option<Block<'static>>>, Error = Simple<char>> {
     let player_default: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(vec![]));
@@ -26,7 +26,7 @@ pub fn parser() -> impl Parser<char, Vec<Option<Block<'static>>>, Error = Simple
                     .push(varn.to_string());
                 None
             });
-    
+
         type_command
     };
 
@@ -46,18 +46,14 @@ pub fn parser() -> impl Parser<char, Vec<Option<Block<'static>>>, Error = Simple
         let _location = text::keyword("loc")
             .ignore_then(
                 number
-                    .clone()
                     .separated_by(just(','))
                     .allow_trailing()
                     .padded()
                     .collect::<Vec<_>>()
                     .padded()
-                    .delimited_by(just('('), just(')'))
-            ).map(|f| {
-                if f.len() != 3 && f.len() != 5 {
-                    
-                }
-            });
+                    .delimited_by(just('('), just(')')),
+            )
+            .map(|f| if f.len() != 3 && f.len() != 5 {});
 
         text.or(number)
     };
@@ -75,16 +71,24 @@ pub fn parser() -> impl Parser<char, Vec<Option<Block<'static>>>, Error = Simple
                     .collect::<Vec<_>>()
                     .padded()
                     .delimited_by(just('('), just(')'))
-                    .padded()
+                    .padded(),
             )
             .padded()
             .map(|(f, datas): (String, Vec<ItemData>)| {
                 let mut items: Vec<Item> = vec![];
                 for (slot, data) in datas.into_iter().enumerate() {
                     let mut id = "".to_string();
-                    if let ItemData::Number { data: _ } = data { id = "num".to_string(); }
-                    if let ItemData::Text { data: _ } = data { id = "txt".to_string(); }
-                    items.push(Item { id, slot: slot.try_into().expect("failed ot convert to usize"), item: data })
+                    if let ItemData::Number { data: _ } = data {
+                        id = "num".to_string();
+                    }
+                    if let ItemData::Text { data: _ } = data {
+                        id = "txt".to_string();
+                    }
+                    items.push(Item {
+                        id,
+                        slot: slot.try_into().expect("failed ot convert to usize"),
+                        item: data,
+                    })
                 }
                 Some(Block::Code {
                     block: "player_action",
@@ -98,20 +102,24 @@ pub fn parser() -> impl Parser<char, Vec<Option<Block<'static>>>, Error = Simple
             .ignore_then(ident)
             .then(
                 arguments
-                    .clone()
+                    
                     .separated_by(just(',').padded())
                     .allow_trailing()
                     .padded()
                     .collect::<Vec<_>>()
                     .padded()
                     .delimited_by(just('('), just(')'))
-                    .padded()
+                    .padded(),
             )
             .padded()
             .map(|(f, datas): (String, Vec<ItemData>)| {
                 let mut items: Vec<Item> = vec![];
                 for (slot, data) in datas.into_iter().enumerate() {
-                    items.push(Item { id: "item".to_string(), slot: slot.try_into().expect("failed ot convert to usize"), item: data })
+                    items.push(Item {
+                        id: "item".to_string(),
+                        slot: slot.try_into().expect("failed ot convert to usize"),
+                        item: data,
+                    })
                 }
                 Some(Block::Code {
                     block: "game_action",
@@ -155,8 +163,6 @@ pub fn parser() -> impl Parser<char, Vec<Option<Block<'static>>>, Error = Simple
 
         player_event
     };
-
-    
 
     events
 }
