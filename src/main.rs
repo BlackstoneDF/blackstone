587cb13e056d90b421090412e66e9c60ac6bf99f
akarahdev <118059229+akarahdev@users.noreply.github.com>
2023-04-28 15:25:44 -0400

    fixed some logic errors and updated executable name

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,8 @@ fn main() -> std::io::Result<()> {
         } else if arg.contains("version") {
             //find the toml file - that has the version
             //3rd line has version as `version = "[version]"
-            match std::fs::read_to_string("Cargo.toml")?.lines().nth(2) {
+            let cargo_toml = include_str!("../Cargo.toml");
+            match cargo_toml.lines().nth(2) {
                 Some(line) => {
                     let line = line.strip_prefix("version = ").unwrap_or("");
                     if line.is_empty() {
@@ -67,6 +68,8 @@ fn main() -> std::io::Result<()> {
                     ))
                 }
             }
+        }  else if arg.contains("help") { help_message(); } else {
+            help_message()
         }
     } else {
         help_message();
@@ -119,6 +122,7 @@ fn process_inputs(input: &str, path: &str) {
     }
 }
 fn help_message() {
+    shulker_header();
     println!(
         r#"
 Blackstone's compiler & build tooling
@@ -132,9 +136,27 @@ Built-in commands:
     build-all               Builds all code in the `scripts` directory & sends it via `recode` mod.
     build-stdout [script]   Sends the code data to the console instead of to `recode`.
                             Useful if you don't have `recode` installed.
-    build_test              Run the tests in the code.
+    build-test              Run the tests in the code.
     add [package]           Add an external package to your scripts.
     help                    Shows this message!
     "#
     );
+}
+
+fn shulker_header() {
+    println!(r#"
+    .dP"Y8 88  88 88   88 88     88  dP 888888 88""Yb 
+    `Ybo." 88  88 88   88 88     88odP  88__   88__dP 
+    o.`Y8b 888888 Y8   8P 88  .o 88"Yb  88""   88"Yb  
+    8bodP' 88  88 `YbodP' 88ood8 88  Yb 888888 88  Yb 
+    "#);
+}
+
+fn blackstone_header() {
+    println!(r#"
+    88""Yb 88        db     dP""b8 88  dP .dP"Y8 888888  dP"Yb  88b 88 888888 
+    88__dP 88       dPYb   dP   `" 88odP  `Ybo."   88   dP   Yb 88Yb88 88__   
+    88""Yb 88  .o  dP__Yb  Yb      88"Yb  o.`Y8b   88   Yb   dP 88 Y88 88""   
+    88oodP 88ood8 dP""""Yb  YboodP 88  Yb 8bodP'   88    YbodP  88  Y8 888888 
+    "#);
 }
